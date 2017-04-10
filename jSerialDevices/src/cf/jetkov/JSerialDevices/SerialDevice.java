ceed5825332462957a2ac3cd59754d059fb238f9
jetkov <jetkov@HDX>
2017-04-10 10:53:01 -0400

    Adjusted oopiness

--- jSerialDevices/src/cf/jetkov/JSerialDevices/SerialDevice.java
+++ jSerialDevices/src/cf/jetkov/JSerialDevices/SerialDevice.java
@@ -5,78 +5,93 @@ import com.fazecast.jSerialComm.*;
 abstract class SerialDevice {
 
 	private SerialPort serialPort;
-
 	private int baudRate;
 
 	public SerialDevice() {
 	}
-	
+
 	public SerialDevice(SerialPort serialPort) {
 		this.serialPort = serialPort;
 	}
-	
+
 	public SerialDevice(SerialPort serialPort, int baudRate) {
 		this.serialPort = serialPort;
 		this.baudRate = baudRate;
 		serialPort.setBaudRate(this.baudRate);
 	}
-	
+
 	public SerialDevice(String description) {
-		setPortMatching(description);
+		serialPort = getPortMatching(description);
+		if (serialPort == null) {
+			System.err.println("ERROR: Port not found!");
+			System.exit(1);
+		}
 	}
-	
+
 	public SerialDevice(String description, int baudRate) {
-		setPortMatching(description);
-		this.baudRate = baudRate;
-		serialPort.setBaudRate(this.baudRate);
+		serialPort = getPortMatching(description);
+		if (serialPort == null) {
+			System.err.println("ERROR: Port not found! Cannot set baud rate.");
+		} else {
+			this.baudRate = baudRate;
+			serialPort.setBaudRate(this.baudRate);
+		}
 	}
-	
-	public boolean setPortMatching(String description) {
+
+	public SerialPort getPortMatching(String description) {
 		String descriptivePortName;
-		
+
 		for (SerialPort port : SerialPort.getCommPorts()) {
 			descriptivePortName = port.getDescriptivePortName();
 			if (descriptivePortName.contains(description)) {
-				serialPort = port;
-				return true;
-			} 		
+				return port;
+			}
 		}
-		System.out.println("No port with description matching '" + description + "' found!");
-		return false;
+		System.err.println("No port with description matching '" + description + "' found!");
+		return null;
 	}
-	
+
 	public void setBaudRate(int baudRate) {
 		this.baudRate = baudRate;
+		serialPort.setBaudRate(this.baudRate);
 	}
-	
+
 	public int getBaudRate() {
 		return baudRate;
 	}
-	
+
 	public void setSerialPort(SerialPort serialPort) {
 		this.serialPort = serialPort;
 	}
-	
+
 	public SerialPort getSerialPort() {
 		return serialPort;
 	}
 
 	public boolean openPort() {
-		if(serialPort.openPort()){
-			try {Thread.sleep(100);} catch(Exception e){}
+		if (serialPort.openPort()) {
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			return true;
-		}
-		else {
+		} else {
 			System.out.println("Error Connecting! Try Another port.");
 			return false;
 		}
 	}
-	
+
 	public void closePort() {
 		serialPort.closePort();
 	}
-	
+
+	public abstract String serialRead();
+
+	public abstract String serialRead(int lines);
+
 	public abstract void serialWrite(String strng);
-	
+
 	public abstract void serialWrite(char chr);
+
 }
