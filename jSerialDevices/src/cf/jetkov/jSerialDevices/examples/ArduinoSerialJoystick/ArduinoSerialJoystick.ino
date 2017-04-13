c920b2364fe34530edd65b1423dbf00125ae0f2b
Alex Petkovic <ajetkov@gmail.com>
2017-04-13 00:21:21 -0400

    Improvements on the correct stack

--- jSerialDevices/src/cf/jetkov/jSerialDevices/examples/ArduinoSerialJoystick/ArduinoSerialJoystick.ino
+++ jSerialDevices/src/cf/jetkov/jSerialDevices/examples/ArduinoSerialJoystick/ArduinoSerialJoystick.ino
@@ -1,24 +1,34 @@
-int xPort = A0;
+// Mapping the physical arduino ports to int variables
+int xPort = A0; // Analog pin 0
 int yPort = A1;
 
+// Coordinate point for the joystick position
 int x, y;
 
+// Setting up serial comms
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(50);
 }
 
+// Iterates forever
 void loop() {
-
-  // read the analog in values
+  // Read in the analog values
   x = analogRead(xPort);
   y = analogRead(yPort);
 
-  // scale to 0 - 1000
+  // Scale to 0-1000 instead of 0-1024
   x = (x/1024.0) * 1000;
   y = (y/1024.0) * 1000;
 
-  // serially print the results
+  // Set the origin to middle instead of top left
+  x -= 500;
+  y -= 500;
+
+  // Fixing y-axis inversion
+  y = -y;
+
+  // Serially sending the results in a framed String format with "/" delimiter
   char buf[12];
   sprintf(buf, "<%d,%d>/", x, y);
   Serial.print(buf);
