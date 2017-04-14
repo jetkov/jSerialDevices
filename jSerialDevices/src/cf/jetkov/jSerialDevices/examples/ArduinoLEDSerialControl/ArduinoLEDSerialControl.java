bcc6c38bd1559911a79da2641825bfd19652e03e
Alex Petkovic <ajetkov@gmail.com>
2017-04-14 00:17:50 -0400

    commenting

--- jSerialDevices/src/cf/jetkov/jSerialDevices/examples/ArduinoLEDSerialControl/ArduinoLEDSerialControl.java
+++ jSerialDevices/src/cf/jetkov/jSerialDevices/examples/ArduinoLEDSerialControl/ArduinoLEDSerialControl.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 import cf.jetkov.jSerialDevices.Arduino;
 
+/**
+ * A simple example of serial communication, using a java console input to control the
+ * state of an LED wired to the Arduino. 
+ * 
+ * @author Alex
+ *
+ */
 public class ArduinoLEDSerialControl {
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
 		Arduino arduino = new Arduino(); // defaults to 9600 baud
 
