684a3d631b252e129c52169845875f23aa6ad2dd
Alex Petkovic <ajetkov@gmail.com>
2017-04-14 00:13:38 -0400

    Commenting

--- jSerialDevices/src/cf/jetkov/jSerialDevices/ArduinoCH340.java
+++ jSerialDevices/src/cf/jetkov/jSerialDevices/ArduinoCH340.java
@@ -1,8 +1,8 @@
 package cf.jetkov.jSerialDevices;
 
 /**
- * Allows for communications to and from a USB connected arduino device in a
- * very abstracted and easily understandable fashion. Extends the
+ * Allows for communications to and from a USB connected arduino device using a CH340
+ * comms chipset in a very abstracted and easily understandable fashion. Extends the
  * {@link SerialDevice} class, utilizing the jSerialComm library.
  * 
  * @author Alex Petkovic
@@ -12,21 +12,21 @@ package cf.jetkov.jSerialDevices;
 public class ArduinoCH340 extends Arduino {
 
 	/**
-	 * Creates a new arduino. By default, chooses a serial port that matches an
-	 * 'arduino' (for windows) or 'ACM' (on linux) keyword. Set's the arduino's
-	 * private serial port to serial device class's serial port.
+	 * Creates a new arduino. By default, chooses a serial port that matches an 'arduino'
+	 * (for windows) or 'ACM' (on linux) keyword. Set's the arduino's private serial port
+	 * to serial device class's serial port.
 	 */
 	public ArduinoCH340() {
 		super("(CH340|ACM)");
 	}
 
 	/**
-	 * Creates a new arduino. By default, chooses a serial port that matches an
-	 * 'arduino' (for windows) or 'ACM' (on linux) keyword--and sets the baud
-	 * rate as specified. Set's the arduino's private serial port to serial
-	 * device class's serial port.
+	 * Creates a new arduino. By default, chooses a serial port that matches an 'arduino'
+	 * (for windows) or 'ACM' (on linux) keyword--and sets the baud rate as specified.
+	 * Set's the arduino's private serial port to serial device class's serial port.
 	 * 
-	 * @param baudRate - The desired baud rate for this serial port
+	 * @param baudRate
+	 *            - The desired baud rate for this serial port
 	 */
 	public ArduinoCH340(int baudRate) {
 		super("(CH340|ACM)", baudRate);
